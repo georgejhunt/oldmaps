4ff9562d794c800893ca24d4230adc3adf20e581
George Hunt <georgejhunt@gmail.com>
2019-05-24 12:54:16 -0700

    sat box -- put not yet working

--- osm-source/vector-console/main.js
+++ osm-source/vector-console/main.js
@@ -1,45 +1,59 @@
-// main.js for Admin Console -- selection of regions
+// main.js for Admin Console using vector tiles for more detail
 import {Fill, Stroke, Style} from 'ol/style';
 import 'ol/ol.css';
-import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import VectorLayer from 'ol/layer/Vector';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
+import MVT from 'ol/format/MVT';
 import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
 import View from 'ol/View';
-//import XYZSource from 'ol/source/XYZ';
-//import MVT from 'ol/format/MVT';
+import {fromLonLat,toLonLat} from 'ol/proj';
+import stylefunction from 'ol-mapbox-style/stylefunction';
+import {add} from 'ol/coordinate';
+import Feature from 'ol/Feature';
+import Polygon from 'ol/geom/Polygon';
+
 
 // a global variable to control which features are shown
+window.$ = window.jQuery = require('jquery');
 var show = {};
-var mapData = "/osm-vector/maplist/assets";
+var mapData = "/admin/map";
+var zoom = 3;
+var lat = 37;
+var lon = -122;
+var radius = 150000;
 
-
-var map = new Map({
-  target: 'map-container',
-  layers: [
-    new VectorLayer({
-      source: new VectorSource({
-        format: new GeoJSON(),
-        url: mapData + '/countries.json'
-      }),
-      style: new Style({
-        fill: new Fill({
-          color: 'rgb(219, 180, 131)' 
-        }),
-        stroke: new Stroke({
-          color: 'white'
-        })
-      })
-    }),
-    
-  ],
+var map = new Map({ target: 'map-container',
   view: new View({
-    center: [0, 0],
+    center: fromLonLat([-71.06, 42.37]),
     zoom: 2
   })
+}); //end of new Map
+window.map = map
+var view = map.getView();
+
+
+var detail = new VectorTileLayer({
+   source: new VectorTileSource({
+      format: new MVT(),
+      url: `./tileserver.php/detail/{z}/{x}/{y}.pbf`,
+      minZoom: 0,
+      attributions: ['&copy <a href="https://openstreetmap.org">OpenStreetMaps, </a> <a href="https://openmaptiles.com"> &copy OpenMapTiles</a>'
+      ],
+      maxZoom: 14
+   }),
+   //declutter: true,
 });
 
-window.map = map
+fetch(mapData + '/style-osm.json').then(function(response) {
+   response.json().then(function(glStyle) {
+     stylefunction(detail, glStyle,"openmaptiles");
+   });
+});
+
+map.addLayer(detail);
 
 var setBoxStyle = function(feature) {
   var name = feature.get("name");
@@ -77,6 +91,18 @@ var boxLayer = new VectorLayer({
 });
 map.addLayer(boxLayer);
 
+var satLayer = new VectorLayer({
+        source: new VectorSource({
+            features: [new Feature({
+                geometry: new Polygon(markers, 'XY'),
+
+            })]
+        })
+        //style: iconStyle
+});
+
+map.addLayer(satLayer);
+
 $( document ).on("mouseover",".extract",function(){
 
   var data = JSON.parse($(this).attr('data-region'));
@@ -89,3 +115,28 @@ $( document ).on("mouseout",".extract",function(){
   show = '';
   boxLayer.changed();
 });
+function update_satbox(evt){
+   //console.log(evt.coordinate)
+   // need to change the following to correct projection
+   var nwcoord = add(evt.coordinate,[-radius,-radius])
+   var secoord = add(evt.coordinate,[radius,radius])
+   satLayer.removeAllFeatures();
+   west = nwcoord[0];
+   south = secoord[0];
+   east = secoord[1];
+   north = nwcoord[1];
+   boxcoords = [(west,north),(west,south),(east,south),(east,north),(west,north);   
+   //console.log(nwcoord);
+   //console.log(secoord);
+   
+}
+map.on("pointermove", function(evt) {
+   var coords = toLonLat(evt.coordinate);
+   lat = coords[1];
+   lon = coords[0];
+   update_satbox(evt);
+});
+$( document ).on('change','#area-choice',function(elem){
+   if ( elem.target.value == 'small' )
+      radius = 50000; else radius = 150000;
+})

aa52b22c529147a863b950f215d3933ae598d667
George Hunt <georgejhunt@gmail.com>
2019-05-26 08:17:33 -0700

    bbox works in packet but not in pack

--- osm-source/vector-console/main.js
+++ osm-source/vector-console/main.js
@@ -24,15 +24,8 @@ var zoom = 3;
 var lat = 37;
 var lon = -122;
 var radius = 150000;
-
-var map = new Map({ target: 'map-container',
-  view: new View({
-    center: fromLonLat([-71.06, 42.37]),
-    zoom: 2
-  })
-}); //end of new Map
-window.map = map
-var view = map.getView();
+var boxcoords = [[[0,0],[0,1],[1,1],[1,0],[0,0]]];
+var ptrcoords = [0,0];
 
 
 var detail = new VectorTileLayer({
@@ -44,7 +37,7 @@ var detail = new VectorTileLayer({
       ],
       maxZoom: 14
    }),
-   //declutter: true,
+   declutter: true,
 });
 
 fetch(mapData + '/style-osm.json').then(function(response) {
@@ -53,7 +46,6 @@ fetch(mapData + '/style-osm.json').then(function(response) {
    });
 });
 
-map.addLayer(detail);
 
 var setBoxStyle = function(feature) {
   var name = feature.get("name");
@@ -89,19 +81,47 @@ var boxLayer = new VectorLayer({
   id: 'boxes',
   style: setBoxStyle
 });
-map.addLayer(boxLayer);
+
+function get_box_coords(radius,lon,lat){
+   // go to polar coords -- Math functions want and provide in radians
+   var deltaY = Math.asin(radius / 6378000.0);
+   //console.log('deltaYradians'+deltaY);
+   var lat_rad = lat * Math.PI /180;
+   var deltaX = deltaY / Math.cos(lat_rad);
+   var lon_rad = lon * Math.PI / 180;
+   var west = (lon_rad - deltaX) * 180 / Math.PI;
+   var south = (lat_rad - deltaY) * 180 / Math.PI;
+   var east = (lon_rad + deltaX) * 180 / Math.PI
+   var north = (lat_rad + deltaY) * 180 / Math.PI
+   console.log('west:'+west+'south:'+south+'east:'+east+'north;'+north)
+   var sw = [west,south];
+   var se = [east,south];
+   var ne = [east,north];
+   var nw = [west,north];
+   sw = fromLonLat(sw);
+   se = fromLonLat(se);
+   ne = fromLonLat(ne);
+   nw = fromLonLat(nw);
+   console.log('box x:' + sw[0]-se[0] + 'box y:' + ne[1]-se[1]);
+   boxcoords = [nw,sw,se,ne,nw];   
+   console.log(boxcoords + 'boxcoords');
+   return(boxcoords);
+}
+var box_spec = get_box_coords(radius,-122.24,37.45);
 
 var satLayer = new VectorLayer({
-        source: new VectorSource({
-            features: [new Feature({
-                geometry: new Polygon(markers, 'XY'),
-
-            })]
-        })
-        //style: iconStyle
+  style: new Style({
+      stroke: new Stroke({
+        color: 'rgb(255, 140, 0, 1)'
+      })
+  }), 
+  source: new VectorSource({
+     features: [new Feature({
+        geometry: new Polygon(box_spec)
+     })]
+  })
 });
 
-map.addLayer(satLayer);
 
 $( document ).on("mouseover",".extract",function(){
 
@@ -115,28 +135,28 @@ $( document ).on("mouseout",".extract",function(){
   show = '';
   boxLayer.changed();
 });
-function update_satbox(evt){
-   //console.log(evt.coordinate)
-   // need to change the following to correct projection
-   var nwcoord = add(evt.coordinate,[-radius,-radius])
-   var secoord = add(evt.coordinate,[radius,radius])
-   satLayer.removeAllFeatures();
-   west = nwcoord[0];
-   south = secoord[0];
-   east = secoord[1];
-   north = nwcoord[1];
-   boxcoords = [(west,north),(west,south),(east,south),(east,north),(west,north);   
-   //console.log(nwcoord);
-   //console.log(secoord);
-   
-}
+
+
+var map = new Map({ target: 'map-container',
+  layers: [detail,boxLayer,satLayer],
+  //layers: [satLayer],
+  view: new View({
+    center: fromLonLat([-122.24,37.45]),
+    zoom: 2
+  })
+}); //end of new Map
+window.map = map
+var view = map.getView();
+
 map.on("pointermove", function(evt) {
-   var coords = toLonLat(evt.coordinate);
-   lat = coords[1];
-   lon = coords[0];
-   update_satbox(evt);
+   ptrcoords = toLonLat(evt.coordinate);
+   lat = ptrcoords[1];
+   lon = ptrcoords[0];
+   //satLayer.getSource().clear();
+   //update_satbox(evt);
 });
 $( document ).on('change','#area-choice',function(elem){
    if ( elem.target.value == 'small' )
       radius = 50000; else radius = 150000;
 })
+
